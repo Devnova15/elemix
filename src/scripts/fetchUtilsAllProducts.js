99a70c2dcfb3ed5f407315bb58bb8b812ba8bdf1
Yaroslav <yaroslavmahey2006@gmail.com>
2024-09-02 21:47:59 +0300

    add fetch for products

---
+++ src/scripts/fetchUtilsAllProducts.js
@@ -0,0 +1,47 @@
+const BASE_URL = 'https://dummyjson.com';
+const PRODUCTS_ENDPOINT = '/products';
+
+fetch(`${BASE_URL}${PRODUCTS_ENDPOINT}`)
+    .then(res => res.json())
+    .then(console.log);
+
+
+export const firstPageProducts = fetch('https://dummyjson.com/products?limit=30&skip=0')
+    .then(res => res.json())
+    .then(data => data.products)
+    .catch(error => {
+        console.error('Error fetching first page products', error);
+        return [];
+    });
+
+export const secondPageProducts = fetch('https://dummyjson.com/products?limit=30&skip=30')
+    .then(res => res.json())
+    .then(data => data.products)
+    .catch(error => {
+        console.error('Error fetching second page products', error);
+        return [];
+    });
+
+export const thirdPageProducts = fetch('https://dummyjson.com/products?limit=30&skip=60')
+    .then(res => res.json())
+    .then(data => data.products)
+    .catch(error => {
+        console.error('Error fetching third page products', error);
+        return [];
+    });
+
+export const fourthPageProducts = fetch('https://dummyjson.com/products?limit=30&skip=90')
+    .then(res => res.json())
+    .then(data => data.products)
+    .catch(error => {
+        console.error('Error fetching fourth page products', error);
+        return [];
+    });
+
+export const fifthPageProducts = fetch('https://dummyjson.com/products?limit=30&skip=120')
+    .then(res => res.json())
+    .then(data => data.products)
+    .catch(error => {
+        console.error('Error fetching fifth page products', error);
+        return [];
+    });
