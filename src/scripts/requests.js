bb242e887b439b934555d1c2cc9cdbb06e4adb69
Yaroslav <yaroslavmahey2006@gmail.com>
2024-09-03 17:20:36 +0300

    do request an products, login, search products

--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,11 +1,44 @@
-const API = 'https://dummyjson.com';
-const PRODUCTS_ENDPOINT = '/products';
+export const API = 'https://dummyjson.com';
+export const PRODUCTS_ENDPOINT = '/products';
+export const PRODUCTS_SEARCH_ENDPOINT = '/products/search';
+export const AUTH_ENDPOINT = '/auth/login';
+
 
 fetch(`${API}${PRODUCTS_ENDPOINT}`)
     .then(res => res.json())
     .then(console.log);
 
+fetch(`${API}${PRODUCTS_SEARCH_ENDPOINT}?q=phone`)
+    .then(res => res.json())
+    .then(console.log);
 
+export const loginDetails = {
+    username: 'emilys',
+    password: 'emilyspass',
+    expiresInMins: 30,
+}
+
+// const jsonString = JSON.stringify(loginDetails);
+// console.log(jsonString);
+// '{"username":"emilys","password":"emilyspass","expiresInMins":30}'
+
+fetch(`${API}${AUTH_ENDPOINT}`,{
+    method : 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(loginDetails),
+})
+    .then(res => res.json())
+    .then(data => {
+        // console.log('user logged in', data);
+        const token = data.token;
+        console.log(token)
+    })
+    .catch(error => console.error('Error:', error));
+
+
+// ДЛЯ ПОЛУЧЕНИЯ ВСЕХ ПРОДУКТОВ в количестве по 30///
 export const firstPageProducts = fetch('https://dummyjson.com/products?limit=30&skip=0')
     .then(res => res.json())
     .then(data => data.products)
@@ -45,3 +78,4 @@ export const fifthPageProducts = fetch('https://dummyjson.com/products?limit=30&
         console.error('Error fetching fifth page products', error);
         return [];
     });
+// ДЛЯ ПОЛУЧЕНИЯ ВСЕХ ПРОДУКТОВ
