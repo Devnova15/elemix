d2e6c4cc5e2aad6dfb2c7f7a6a4f705ab1d848d7
Yaroslav <yaroslavmahey2006@gmail.com>
2024-09-04 12:43:18 +0300

    reworked the queries, add Endpoints

---
+++ src/scripts/constants.js
@@ -0,0 +1,21 @@
+
+export const API = 'https://fakestoreapi.com';
+
+export const ENDPOINTS = Object.freeze({
+    PRODUCTS: '/products',
+    CARTS: '/carts',
+    USERS: '/users',
+    AUTH: '/auth',
+});
+
+export const NESTED_AUTH_ENDPOINT = Object.freeze({
+    LOGIN: '/login'
+});
+
+export const loginDetails = Object.freeze({
+    username: 'emilys',
+    password: 'emilyspass',
+    expiresInMins: 30,
+});
+
+
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,81 +1,30 @@
-export const API = 'https://dummyjson.com';
-export const PRODUCTS_ENDPOINT = '/products';
-export const PRODUCTS_SEARCH_ENDPOINT = '/products/search';
-export const AUTH_ENDPOINT = '/auth/login';
 
+import { API, ENDPOINTS, NESTED_AUTH_ENDPOINT, loginDetails} from './constants.js';
 
-fetch(`${API}${PRODUCTS_ENDPOINT}`)
-    .then(res => res.json())
-    .then(console.log);
+const sendRequest = async (url, method = "GET", options = {}) => {
+    try {
+        const response = await fetch(url, {
+            method,
+            ...options,
+        });
+        return await response.json();
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
+};
 
-fetch(`${API}${PRODUCTS_SEARCH_ENDPOINT}?q=phone`)
-    .then(res => res.json())
-    .then(console.log);
-
-export const loginDetails = {
-    username: 'emilys',
-    password: 'emilyspass',
-    expiresInMins: 30,
+const searchProducts = async (item) => {
+    return await sendRequest(`${API}${ENDPOINTS.PRODUCTS}/search?q=${item}`)
 }
 
-// const jsonString = JSON.stringify(loginDetails);
-// console.log(jsonString);
-// '{"username":"emilys","password":"emilyspass","expiresInMins":30}'
+const loginUsers = async () => {
+    return await sendRequest(`${API}${ENDPOINTS.AUTH}${NESTED_AUTH_ENDPOINT.LOGIN}`, {
+        method : 'POST',
+        headers : {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(loginDetails)
 
-fetch(`${API}${AUTH_ENDPOINT}`,{
-    method : 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(loginDetails),
-})
-    .then(res => res.json())
-    .then(data => {
-        // console.log('user logged in', data);
-        const token = data.token;
-        console.log(token)
     })
-    .catch(error => console.error('Error:', error));
-
-
-// ДЛЯ ПОЛУЧЕНИЯ ВСЕХ ПРОДУКТОВ в количестве по 30///
-export const firstPageProducts = fetch('https://dummyjson.com/products?limit=30&skip=0')
-    .then(res => res.json())
-    .then(data => data.products)
-    .catch(error => {
-        console.error('Error fetching first page products', error);
-        return [];
-    });
-
-export const secondPageProducts = fetch('https://dummyjson.com/products?limit=30&skip=30')
-    .then(res => res.json())
-    .then(data => data.products)
-    .catch(error => {
-        console.error('Error fetching second page products', error);
-        return [];
-    });
-
-export const thirdPageProducts = fetch('https://dummyjson.com/products?limit=30&skip=60')
-    .then(res => res.json())
-    .then(data => data.products)
-    .catch(error => {
-        console.error('Error fetching third page products', error);
-        return [];
-    });
-
-export const fourthPageProducts = fetch('https://dummyjson.com/products?limit=30&skip=90')
-    .then(res => res.json())
-    .then(data => data.products)
-    .catch(error => {
-        console.error('Error fetching fourth page products', error);
-        return [];
-    });
-
-export const fifthPageProducts = fetch('https://dummyjson.com/products?limit=30&skip=120')
-    .then(res => res.json())
-    .then(data => data.products)
-    .catch(error => {
-        console.error('Error fetching fifth page products', error);
-        return [];
-    });
-// ДЛЯ ПОЛУЧЕНИЯ ВСЕХ ПРОДУКТОВ
+};
