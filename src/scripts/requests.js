9b7ecc18d26681a2b1ab2fddb42940d88c17cd0f
Yaroslav <yaroslavmahey2006@gmail.com>
2024-09-04 14:59:09 +0300

    add new request getAllCarts & getSingleCart

--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -18,6 +18,8 @@ const searchProducts = async (item) => {
     return await sendRequest(`${API}${ENDPOINTS.PRODUCTS}/search?q=${item}`)
 }
 
+//getAllUsers тоже пагинация
+
 const loginUsers = async () => {
     return await sendRequest(`${API}${ENDPOINTS.AUTH}${NESTED_AUTH_ENDPOINT.LOGIN}`, {
         method : 'POST',
@@ -28,3 +30,11 @@ const loginUsers = async () => {
 
     })
 };
+
+const getAllCarts = async () => {
+    return await sendRequest(`${API}${ENDPOINTS.CARTS}`)
+}
+
+const getSingleCart = async (productId) => {
+    return await sendRequest(`${API}${ENDPOINTS}/${productId}`)
+}
